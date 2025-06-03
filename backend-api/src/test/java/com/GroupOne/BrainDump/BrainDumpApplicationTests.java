ef15e4437fd077050fb947c381319efab872b987
Sebastian-F2030 <stforeman@uncg.edu>
2025-06-03 17:15:02 -0400

    initialize spring boot framework and design document examples

---
+++ backend-api/src/test/java/com/GroupOne/BrainDump/BrainDumpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GroupOne.BrainDump;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrainDumpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
