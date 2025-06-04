aea5a857353876ce16858f80cb490ebcb03f0d51
EmiliaAT <latrickett@uncg.edu>
2025-06-04 17:10:28 -0400

    Implement User Entity fields

--- server/src/main/java/com/csc340_group_one/brain_dump/user/User.java
+++ server/src/main/java/com/csc340_group_one/brain_dump/user/User.java
@@ -1,5 +1,7 @@
 package com.csc340_group_one.brain_dump.user;
 
+import java.time.Instant;
+import java.util.Optional;
 import java.util.Set;
 
 import com.csc340_group_one.brain_dump.article.Article;
@@ -10,16 +12,24 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
 @Data
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
+@RequiredArgsConstructor
 @Table(name = "users")
 public class User {
 
@@ -28,15 +38,46 @@ public class User {
     @Column(name = "user_id", nullable = false)
     private Long id;
 
+    @NonNull
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @NonNull
+    @Column(name = "username", nullable = false, unique = true)
+    private String username;
+
+    @NonNull
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @NonNull
+    @Column(name = "created_by", nullable = false)
+    private Instant createdBy;
+
+    @NonNull
+    @Column(name = "modified_by", nullable = false)
+    private Instant modifiedBy;
+
+    @NonNull
     @OneToMany(mappedBy = "author")
     private Set<Article> articles;
 
+    @NonNull
     @OneToMany(mappedBy = "author")
     private Set<Collection> collections;
 
-    public User(Set<Article> articles, Set<Collection> collections) {
-        this.articles = articles;
-        this.collections = collections;
+    @Getter(value = AccessLevel.NONE)
+    @Setter(value = AccessLevel.NONE)
+    @OneToOne(optional = true)
+    @JoinColumn(name = "homepage_id", nullable = true)
+    private Article homepage;
+
+    public Optional<Article> getHomepage() {
+        return Optional.ofNullable(this.homepage);
+    }
+
+    public void setHomepage(Article article) {
+        this.homepage = article;
     }
 
 }
